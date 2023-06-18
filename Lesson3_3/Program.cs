0f2cc891b9eb92c153d66981493c00e19278a1f3
RomaMed <pl.py645@gmail.com>
2023-06-18 22:10:30 +1100

    1

---
+++ Lesson3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number;
+int index = 1;
+int result;
+Console.Write("Введите число: ");
+number= Int32.Parse(Console.ReadLine());
+
+while (index < number + 1)
+{
+    result = index * index * index;
+    index++;
+    Console.Write(result + ", ");
+
+}
