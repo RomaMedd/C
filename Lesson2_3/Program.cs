a62fd5c8e68294e7c7722cc75d3d8bc349a8a1a2
RomaMed <pl.py645@gmail.com>
2023-06-18 17:35:07 +1100

    1

---
+++ Lesson2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int week = Int32.Parse(Console.ReadLine());
+if (week > 7)
+{
+    Console.WriteLine("Введите цифру от 1 до 7");
+}
+else
+{
+    if (week > 5)
+    {
+        Console.WriteLine("Да, выходной");
+    }
+    else
+        Console.WriteLine("Нет, будни");
+}
