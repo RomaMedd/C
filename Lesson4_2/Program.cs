c871b7721259447cd8afedec69501e3480b7dcd0
RomaMed <pl.py645@gmail.com>
2023-06-25 17:48:46 +1100

    1

---
+++ Lesson4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+class Program
+{
+    static int Sum(int value)
+    {
+        if (value < 10)
+        return value;
+
+        int remains = value % 10;
+
+        int nvalue = value / 10;
+
+        return remains + Sum(nvalue);
+
+    }
+
+    static void PrintResult(int result1)
+    {
+        Console.WriteLine("Результаты вычеслений = " + result1);
+    }
+    static void Main (string[] args)
+    {
+        int numbers = Int32.Parse(Console.ReadLine());
+        int result = Sum(numbers);
+        PrintResult(result);
+    }
+
+}
+
