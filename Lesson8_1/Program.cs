477c6dce455e35c4faa1097aa108cb259bcaafd8
RomaMed <pl.py645@gmail.com>
2023-07-19 17:22:59 +1100

    1

---
+++ Lesson8_1/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void SortToLower(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength( 1);j++)
+        {
+            for (int k = 0; k < matr.GetLength(   1)-1;k++)
+            {
+                if (matr[i, k] < matr[i, k + 1])
+                {
+                    int temp = matr[i, k + 1];
+ matr[i, k + 1] = matr[i, k];
+ matr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Console.Write("Введите количество строк: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[i, j];
+
+FillArray(matrix);
+PrintArray(matrix);
+SortToLower(matrix);
+Console.WriteLine();
+PrintArray(matrix);
